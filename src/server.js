84e7aae754eb9fe16d0666fe3828c9f8adddd3e8
wang-hsoo <dhkd4873@naver.com>
2021-12-20 15:47:09 +0900

    first commit

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
+import { on } from "events";
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 
@@ -9,4 +12,18 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/")); 
 const handleListen = () => console.log('Listening on http://localhost:3000');
 
-app.listen(3000, handleListen);
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({server});
+
+
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser");
+    socket.on("close", () => console.log("Disconnceted from the Browser X"));
+    socket.on("message", (message) =>{
+        console.log(message.toString('utf-8'));
+    });
+    socket.send("hello!!");
+});
+
+server.listen(3000, handleListen);
